31747762eebc3c1007579e80a83c8c6ae05c3245
Zacchaeus Ndirangu Mwangi <ndirangu.zmwangi@gmail.com>
2024-05-31 19:43:34 +0300

    feat: Add a delete dunction to the delete button

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -4,11 +4,16 @@ import Task from './Task'
 function TaskList({initialTasks}) {
   const [tasks, setTasks] = useState(initialTasks)
 
+  function handleDeleteTask(e){
+    const newTasks = tasks.filter(task => task.text !== e.target.parentElement.querySelector('.text').textContent)
+    setTasks(newTasks)
+  }
+
   return (
     <div className="tasks">
       {
         tasks.map(task => {
-          return <Task key={Date.now()} text={task.text} category={task.category}/>
+          return <Task key={task.id} text={task.text} category={task.category} handleDelete={handleDeleteTask}/>
         })
       }
     </div>
