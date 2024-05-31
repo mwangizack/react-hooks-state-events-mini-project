ba031bb1fa1173c8fa03609ec026e5b19aac0c3e
Zacchaeus Ndirangu Mwangi <ndirangu.zmwangi@gmail.com>
2024-05-31 18:33:49 +0300

    fix: Use a state variable to display tasks

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Task from './Task'
 
 function TaskList({initialTasks}) {
+  const [tasks, setTasks] = useState(initialTasks)
+
   return (
     <div className="tasks">
       {
-        initialTasks.map(task => {
+        tasks.map(task => {
           return <Task key={Date.now()} text={task.text} category={task.category}/>
         })
       }
